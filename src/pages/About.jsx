80164829291a1f49d4735384252fd805333ae985
Hosea54 <ejohhoseamwongwe@gmail.com>
2025-11-11 14:25:46 -0500

    global page styles

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,24 @@
+import styled from "styled-components";
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <Wrapper>
+      <h3>About us</h3>
+      <p>
+        Meet Barmate - Your Pocket Bartender with Personality! Discover a world
+        of colorful cocktails and clever mocktails pulled straight from the
+        coolest drink APIs around. Whether you are mixing for a chill night in
+        or showing off to friends, Barmate serves up recipes, vibes, and
+        inspiration with every shake. Ready to sip, stir, and sparkle?
+      </p>
+    </Wrapper>
+  );
 };
+const Wrapper = styled.div`
+  p {
+    line-height: 2;
+    color: var(--grey-500);
+    margin-top: 2rem;
+  }
+`;
 export default About;
