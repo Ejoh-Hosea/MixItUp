c24f67ef08a04ee7dec1f0dd3311465b5cfcd66f
Hosea54 <ejohhoseamwongwe@gmail.com>
2025-11-16 21:15:00 -0500

    Search Form styled component complete

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,10 +1,11 @@
 import { Form, useNavigation } from "react-router-dom";
+import styled from "styled-components";
 
 const SearchForm = () => {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
   return (
-    <div>
+    <Wrapper>
       <Form className="form">
         <input
           type="search"
@@ -16,7 +17,22 @@ const SearchForm = () => {
           {isSubmitting ? "searching..." : "search"}
         </button>
       </Form>
-    </div>
+    </Wrapper>
   );
 };
+const Wrapper = styled.div`
+  margin-bottom: 6rem;
+  .form {
+    display: grid;
+    grid-template-columns: 1fr auto;
+  }
+  .form-input {
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+  }
+  .btn {
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+  }
+`;
 export default SearchForm;
